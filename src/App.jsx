dfaa2eb362e1daf00d8acec690a580b0beebcd39
Ritwik <ritwikhero@gmail.com>
2024-03-25 23:59:17 +0530

    Finished with static site

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import "./App.css";
-
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
 function App() {
   return (
   <div>
-    <nav>
-      <div className="logo">
-        <img src="/images/brand_logo.png" alt="logo" />
-      </div>
-      <ul>
-        <li href='#'>Menu</li>
-        <li href='#'>Location</li>
-        <li href='#'>About</li>
-        <li href='#'>Contact</li>
-      </ul>
-      <button>Login</button>
-    </nav>
+    <Navigation/>
+    <Hero/>
   </div>
   );
 }
